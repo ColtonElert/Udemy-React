16c517617f5be3bc6d11bf2af8f4828563734cb8
Colton <coltonelert@gmail.com>
2023-07-10 15:18:06 -0500

    RoboFriends React

---
+++ RoboFriends/src/Robots.js
@@ -0,0 +1,45 @@
+export const robots = [
+    {
+        id: 'Reese',
+        name: 'Reese Newcomb',
+        username: 'nukem52x',
+        email:'reece.nukem@gmail.com'
+    },
+    {
+        id:'Nate',
+        name: 'Nate Vasey',
+        username: 'aggrieveddrip04',
+        email: 'nate.vasey@gmail.com'
+    },
+    {
+        id:'Jeremiah',
+        name: 'Jeremiah Schwarts',
+        username: 'Reddpanda',
+        email:'jeremiah.shwartz@gmail.com'
+    },
+    {
+        id:'Jake',
+        name: 'Jake Wedig',
+        username: 'Resolve',
+        email: 'jake.wedig@gmail.com'
+    },
+    {
+        id:'Rocky',
+        name: 'Rocky Sposato',
+        username: 'Vinsmoke',
+        email:'rocky.sposato@gmail.com'
+    },
+    {
+        id:'Colton',
+        name: 'Colton Elert',
+        username: 'CozMercs',
+        email: 'colton.elert@gmail.com'
+    },
+    {
+        id:'Jess',
+        name: 'Jessica Schuler',
+        username: 'Jessica',
+        email: 'jessica.schuler@gmail.com'
+    }
+
+]
